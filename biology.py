a441d10326861896864fd889f1f79ff0019df24d
Maciej Moryń <moryn.maciej@protonmail.com>
2024-02-25 20:11:48 +0100

    moved Ant and Colony classes into single biology module

--- biology.py
+++ biology.py
@@ -1,5 +1,4 @@
-from DistanceGrid import DistanceGrid
-from PheromoneGrid import PheromoneGrid
+from grid import DistanceGrid, PheromoneGrid
 from random import randrange, random
 
 
@@ -47,3 +46,33 @@ class Ant:
         for possible_destination in possible_destinations:
             pheromone_sum += pheromone_grid.pheromones_between(start, possible_destination)
         return pheromone_sum
+
+
+class Colony:
+    def __init__(self, size: int, distance_grid: DistanceGrid, pheromone_grid: PheromoneGrid):
+        ants = []
+        best_ant = None
+        for index in range(size):
+            ant = Ant(distance_grid, pheromone_grid)
+            if best_ant is None or ant.get_travelled_distance() < best_ant.get_travelled_distance():
+                best_ant = ant
+            ants.append(ant)
+        self.__ants = ants
+        self.__best_ant = best_ant
+
+    def __iter__(self):
+        self.__index = 0
+        return self
+
+    def __next__(self) -> Ant:
+        colony_size = len(self.__ants)
+        if self.__index < colony_size:
+            ant = self.__ants[self.__index]
+            self.__index += 1
+            return ant
+        else:
+            raise StopIteration
+
+    def get_best_ant(self) -> Ant:
+        return self.__best_ant
+
